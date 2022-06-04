c1deabf90d06c2c3b57f32a446cc0d13c42ed3e5
Alex <alexman54d@bk.ru>
2022-06-05 00:05:50 +0300

    project1

---
+++ script.js
@@ -0,0 +1,19 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {
+
+	},
+	actors: {
+
+	},
+	genres: [],
+	privat: false,
+};
+
+personalMovieDB.movies[prompt('Один из просмотренных фильмов?')] = prompt('На сколько оцените его?');
+personalMovieDB.movies[prompt('Один из просмотренных фильмов?')] = prompt('На сколько оцените его?');
+console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
